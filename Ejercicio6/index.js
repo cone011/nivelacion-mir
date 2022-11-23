27d99ba4e3eb1741ab6666354241580108919f84
Juan <jjcaimen@gmail.com>
2022-11-23 20:39:15 -0300

    commit ejercicio 6 bonus

--- Ejercicio6/index.js
+++ Ejercicio6/index.js
@@ -26,3 +26,42 @@ function saludo(nombre) {
 }
 
 console.log(saludo(pedro.nombre));
+
+function productosBaratos(productos) {
+  let arrProducto = [];
+  if (productos.length === 0) return arrProducto;
+  for (const key in productos) {
+    if (productos[key].precio >= 5 && productos[key].precio <= 10) {
+      arrProducto.push(productos[key].nombre);
+    }
+  }
+  return arrProducto;
+}
+
+let prods = [
+  { nombre: "Arroz", precio: 5 },
+  { nombre: "Pan", precio: 3 },
+  { nombre: "Tomate", precio: 8 },
+  { nombre: "Leche", precio: 15 },
+];
+console.log(productosBaratos(prods));
+
+function frecuencias(palabra) {
+  let arrObject = [];
+  for (const key in palabra) {
+    if (palabra[key] !== " ") {
+      if (arrObject.find((item) => item.letra === palabra[key])) {
+        const index = arrObject.findIndex(
+          (item) => item.letra === palabra[key]
+        );
+        arrObject[index].contador = arrObject[index].contador + 1;
+      } else {
+        arrObject.push({ letra: palabra[key], contador: 1 });
+      }
+    }
+  }
+  return arrObject;
+}
+
+console.log(frecuencias("hola mundo"));
+console.log(frecuencias("anita lava la tina"));
